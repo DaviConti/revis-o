ce318b104dbfcce8d3d009891d1190e57f47e80e
Davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-04-04 09:37:56 -0300

    add

---
+++ script.js
@@ -0,0 +1,20 @@
+alert('ola mundo')
+
+function somador() {
+   let total = 0;
+   while (true) {
+       let num = prompt("Digite um número para somar (ou 'sair' para finalizar):");
+       if (num.toLowerCase() === 'sair') {
+           break;
+       }
+       let numero = parseFloat(num);
+       if (!isNaN(numero)) {
+           total += numero;
+       } else {
+           alert("Por favor, digite um número válido.");
+       }
+   }
+   alert("Soma total: " + total);
+}
+
+somador();
