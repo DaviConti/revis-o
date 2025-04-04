1508115b76cda51ae0b5fa606a3cada67617efb3
Davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-04-04 09:52:00 -0300

    add-atividade numero trez

--- script.js
+++ script.js
@@ -37,3 +37,34 @@ function contagemRegressiva() {
 
 contagemRegressiva();
 */
+
+/* terceiro
+function balancoFinanceiro() {
+   let totalGanhos = 0;
+   let totalGastos = 0;
+   
+   for (let i = 1; i <= 12; i++) {
+       let ganho = parseFloat(prompt(`Digite o ganho bruto do mês ${i}:`));
+       let gasto = parseFloat(prompt(`Digite o gasto do mês ${i}:`));
+       
+       if (!isNaN(ganho)) totalGanhos += ganho;
+       if (!isNaN(gasto)) totalGastos += gasto;
+   }
+   
+   let saldoFinal = totalGanhos - totalGastos;
+   
+   console.log("Total de ganhos: R$" + totalGanhos.toFixed(2));
+   console.log("Total de gastos: R$" + totalGastos.toFixed(2));
+   console.log("Saldo final: R$" + saldoFinal.toFixed(2));
+   
+   if (saldoFinal > 0) {
+       console.log("A empresa teve LUCRO este ano!");
+   } else if (saldoFinal < 0) {
+       console.log("A empresa teve PREJUÍZO este ano!");
+   } else {
+       console.log("A empresa ficou no ZERO a ZERO este ano.");
+   }
+}
+
+balancoFinanceiro();
+*/
